e6c4ddc7e7e4537f7d456742ab61b7e775d1ecdb
Gianluc Lui <58863775+glui@users.noreply.github.com>
2022-10-11 21:37:56 +0100

    Fix issue #2 (#3)
    * Indented code.
    
    * Changed push.sh to use external configuration files.
    
    * Check for auth tokens first.
    --> Better Handling of the configuration files
    
    Co-authored-by: glui <g.lui@surrey.ac.uk>

--- jobListener.sh
+++ jobListener.sh
@@ -14,29 +14,25 @@ counter=0
 echo "$(date  +'%d/%m %H:%M') Running $a Jobs."
 
 while [[ $a -gt -1 && counter -lt 20000 ]]; do
-counter=$((counter +1))
-sleep 30
-if [ $a -lt $(squeue | grep $(whoami) | wc -l) ]
-then
-a=$(squeue | grep $(whoami) | wc -l);
-name_of_job=$(squeue | grep $(whoami) | awk '{print $1 "\t" $2"\t" $3}');
-push "$clusterName" "Received Job. Running $a jobs "
-echo "$(date  +'%d/%m %H:%M')   Received Job. Running $a jobs" 
-counter=0       #resets the counter
-fi 
+  counter=$((counter +1))
+  sleep 30
+  if [ $a -lt $(squeue | grep $(whoami) | wc -l) ]; then
+    a=$(squeue | grep $(whoami) | wc -l);
+    name_of_job=$(squeue | grep $(whoami) | awk '{print $1 "\t" $2"\t" $3}');
+    push "$clusterName" "Received Job. Running $a jobs "
+    echo "$(date  +'%d/%m %H:%M')   Received Job. Running $a jobs" 
+    counter=0       #resets the counter
+  fi 
 
-if [ $a -gt $(squeue | grep $(whoami) | wc -l) ] 
-then 
-
-name_remaining=$(squeue | grep $(whoami) | awk '{print $1 "\t" $2"\t" $3}')
-name_of_job_finished=$(printf "$name_of_job\n$name_remaining\n" | sort | uniq -u | awk '{print $3}')
-echo "Job finished: $name_of_job_finished"
-a=$(squeue | grep $(whoami) | wc -l)
-push "$clusterName" "One job has finished ($a remaining). $name_of_job_finished" 
-echo "$(date +'%d/%m %H:%M')   One job has finished: $name_of_job_finished. ($a remaining)"
-name_of_job=$name_remaining
-
-fi
-
+  if [ $a -gt $(squeue | grep $(whoami) | wc -l) ]; then 
+    name_remaining=$(squeue | grep $(whoami) | awk '{print $1 "\t" $2"\t" $3}')
+    name_of_job_finished=$(printf "$name_of_job\n$name_remaining\n" | sort | uniq -u | awk '{print $3}')
+    echo "Job finished: $name_of_job_finished"
+    a=$(squeue | grep $(whoami) | wc -l)
+    push "$clusterName" "One job has finished ($a remaining). $name_of_job_finished" 
+    echo "$(date +'%d/%m %H:%M')   One job has finished: $name_of_job_finished. ($a remaining)"
+    name_of_job=$name_remaining
+  fi
 done
+
 echo 'JobListener is terminating'
--- push.sh
+++ push.sh
@@ -2,14 +2,18 @@
 
 function push {
     if [ "$1" = "-h" ]; then
-    echo " Sends a request to the pushover.net server"
-    echo "Takes two positionnal arguments the title and the body of the push notification"
+        echo " Sends a request to the pushover.net server"
+        echo "Takes two positionnal arguments the title and the body of the push notification"
         return  
     fi
 
-    #requires COSTUMISATION
-    curl -s -F "token=TOKENNUMBER" \
-    -F "user=USERID" \
+    CDIR="${XDG_CONFIG_HOME:-$HOME/.config}/push"
+    mkdir -p "$CDIR"
+    [ ! -f "$CDIR/token" -o ! -f "$CDIR/user_id" ] && \
+        { echo "Auth tokens must be supplied."; return 1; }
+
+    curl -s -F "token="$(<"$CDIR/token") \
+    -F "user="$(<"$CDIR/user_id") \
     -F "title=$1" \
     -F "message=$2" https://api.pushover.net/1/messages.json >/dev/null
 }
